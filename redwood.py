4db5719c21c9f052fb616a05af066d78203b57fd
Henrik Treadup <henrik.treadup@gmail.com>
2016-12-03 23:37:58 +0100

    Added bookmarks.

--- redwood.py
+++ redwood.py
@@ -4,3 +4,20 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/bookmarks')
+def bookmarks():
+
+    pythonBookmarks = [('Django', 'http://www.djangoproject.com'),
+                       ('Flask', ''),
+                       ('Jinja2', ''),
+                       ('Django Rest Framework', ''),
+                       ('Fabric', '')]
+                       
+    clojureBookmarks = [('Clojure', ''),
+                        ('Clojure Toolbox', '')]
+    
+    marks = [{'name':'Python', 'items': pythonBookmarks},
+             {'name':'Clojure', 'items': clojureBookmarks}]
+
+    return render_template('bookmarks.html', bookmarks=marks)
