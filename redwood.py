048e5634bbe781e70bf57331ae3b4e529f086c35
Henrik Treadup <henrik.treadup@gmail.com>
2016-12-07 22:46:48 +0100

    Moved bookmarks out to a json file.

--- redwood.py
+++ redwood.py
@@ -1,23 +1,51 @@
 from flask import Flask, render_template
+import json
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+def load_bookmarks():
+    """
+    Loads the bookmarks from the bookmarks.json file
+    """
+    with open('bookmarks.json') as f:
+        bookmarks_json = f.read()
+
+    return json.loads(bookmarks_json)
+
 @app.route('/bookmarks')
-def bookmarks():
+def bookmarks(bookmarks=load_bookmarks()): # Hack to only load the bookmarks file once.
+    """
+    Show bookmarks belonging to different categories
+    """
+    return render_template('bookmarks.html', bookmarks=bookmarks)
 
-    pythonBookmarks = [('Django', 'http://www.djangoproject.com'),
-                       ('Flask', ''),
-                       ('Jinja2', ''),
-                       ('Django Rest Framework', ''),
-                       ('Fabric', '')]
-                       
-    clojureBookmarks = [('Clojure', ''),
-                        ('Clojure Toolbox', '')]
-    
-    marks = [{'name':'Python', 'items': pythonBookmarks},
-             {'name':'Clojure', 'items': clojureBookmarks}]
+@app.route('/photos')
+def photos():
+    return render_template('photos.html')
 
-    return render_template('bookmarks.html', bookmarks=marks)
+@app.route('/photos/<slug>')
+def photo_collection(slug):
+    return 'Photos go here.'
+
+# Have an index page for image collections.
+# Each image collection should have a page 
+
+@app.route('/notes')
+@app.route('/notes/<path>')
+def notes(path=None):
+    """
+    Shows markdown notes hosted on S3. The notes should be a protected resource.
+    In other words they require authentication to access.
+    """
+    return 'Notes go here.'
+
+@app.route('/hacks')
+def hacks():
+    """
+    List of projects that I have created.
+    """
+    return 'Hacks go here.'
