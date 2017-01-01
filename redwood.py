be6955917cce64cb00bfc0c1651a93575f6703c7
Henrik Treadup <henrik.treadup@gmail.com>
2017-01-01 12:39:03 +0100

    Implemented Photo Collection List page.

--- redwood.py
+++ redwood.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
 import json
+from pprint import pprint
 
 def create_app():
     app = Flask(__name__)
     app.config['BOOKMARKS_FILENAME'] = 'bookmarks.json'
+    app.config['PHOTO_COLLECTION_FILENAME'] = 'photos/photo_collections.json'
     return app
 
 app = create_app()
@@ -12,16 +14,21 @@ app = create_app()
 def index():
     return render_template('index.html')
 
+def load_json(filename):
+    """
+    Loads json from the file with the given filename.
+    """
+    with open(filename) as f:
+        contents = f.read()
+
+    return json.loads(contents)
+
 def load_bookmarks():
     """
     Loads the bookmarks from the bookmarks.json file
     """
     filename = app.config['BOOKMARKS_FILENAME']
-    
-    with open(filename) as f:
-        bookmarks_json = f.read()
-
-    return json.loads(bookmarks_json)
+    return load_json(filename)
 
 @app.route('/bookmarks')
 def bookmarks():
@@ -35,19 +42,51 @@ def bookmarks():
 # In other words have a single collection page and then
 # a detail page for each photo.
 
+def add_collection_thumbnail_url(collection):
+    pprint(collection)
+    f = 'http://s3.amazonaws.com/rainforestphotos/hawaii-2015/thumbs/thumb_{}'
+    collection['thumbnail'] = f.format(collection['image'])
+
+def add_collection_url(collection):
+    collection['url'] = '/photos/{}'.format(collection['slug'])
+    
+def load_photo_collection_list():
+    filename = app.config['PHOTO_COLLECTION_FILENAME']
+    photo_collections =  load_json(filename)
+
+    for collection in photo_collections:
+        add_collection_url(collection)
+        add_collection_thumbnail_url(collection)
+
+    return photo_collections
+    
 @app.route('/photos')
-def photos():
+def photo_collection_list():
     """
     Show list of photo collections.
     """
-    return render_template('photos.html')
+    photo_collections = load_photo_collection_list()
+    return render_template('photo-collection-list.html', photo_collections=photo_collections)
 
-@app.route('/photos/<collection>')
-def photo_collection(collection):
+def get_photo_collection(collection_name):
+    photo_collections = load_photo_collection_list()
+    for collection in photo_collections:
+        if collection['slug'] == collection_name:
+            return collection
+
+    return None
+
+@app.route('/photos/<collection_name>')
+def photo_collection(collection_name):
     """
     Show a single photo collection.
     """
-    return 'Photos go here.'
+    collection = get_photo_collection(collection_name)
+
+    if(collection):
+        return render_template('photo-collection.html', collection=collection)
+    else:
+        return "Could not find the collection. Should return a nice 404 error here."
 
 @app.route('/photos/<collection>/<photo>')
 def single_photo(collection, photo):
