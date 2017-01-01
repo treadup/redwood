3417a293e6d44077783b8efa76f25eca00df979e
Henrik Treadup <henrik.treadup@gmail.com>
2017-01-01 20:03:20 +0100

    Initial implementation of the photo gallery.

--- redwood.py
+++ redwood.py
@@ -42,10 +42,16 @@ def bookmarks():
 # In other words have a single collection page and then
 # a detail page for each photo.
 
+def get_thumbnail_url(collection_name, image_name):
+    f = 'http://s3.amazonaws.com/rainforestphotos/{}/thumbs/thumb_{}'
+    return f.format(collection_name, image_name)
+
+def get_photo_url(collection_name, image_name):
+    f = 'http://s3.amazonaws.com/rainforestphotos/{}/{}'
+    return f.format(collection_name, image_name)
+
 def add_collection_thumbnail_url(collection):
-    pprint(collection)
-    f = 'http://s3.amazonaws.com/rainforestphotos/hawaii-2015/thumbs/thumb_{}'
-    collection['thumbnail'] = f.format(collection['image'])
+    collection['thumbnail'] = get_thumbnail_url(collection['slug'], collection['image'])
 
 def add_collection_url(collection):
     collection['url'] = '/photos/{}'.format(collection['slug'])
@@ -68,7 +74,7 @@ def photo_collection_list():
     photo_collections = load_photo_collection_list()
     return render_template('photo-collection-list.html', photo_collections=photo_collections)
 
-def get_photo_collection(collection_name):
+def get_raw_photo_collection(collection_name):
     photo_collections = load_photo_collection_list()
     for collection in photo_collections:
         if collection['slug'] == collection_name:
@@ -76,6 +82,23 @@ def get_photo_collection(collection_name):
             return load_json(filename)
 
     return None
+
+def map_image(collection_name, image_name):
+    return {"name": image_name,
+            "url": get_photo_url(collection_name, image_name),
+            "thumbnail": get_thumbnail_url(collection_name, image_name)}
+    
+
+def get_photo_collection(collection_name):
+    collection = get_raw_photo_collection(collection_name)
+
+    if collection:
+        collection['images'] = [map_image(collection_name, img) for img in collection['images']]
+        print("Collection contents")
+        pprint(collection)
+        return collection
+    else:
+        return None
 
 @app.route('/photos/<collection_name>')
 def photo_collection(collection_name):
@@ -88,6 +111,9 @@ def photo_collection(collection_name):
     pprint(collection)
 
     if(collection):
+        # Replace the images array with an array of objects with
+        # the following properties.
+        # name, url, thumbnail (which is a url)
         return render_template('photo-collection.html', collection=collection)
     else:
         return "Could not find the collection. Should return a nice 404 error here."
