a8d88e3dd5db5998d02fe15797d2584452c870e5
Henrik Treadup <henrik.treadup@gmail.com>
2017-08-01 16:17:39 +0200

    You can now toggle the navigation menu.

---
+++ static/js/redwood.js
@@ -0,0 +1,12 @@
+// Toggle the navigation menu between displayed and hidden.
+// This only has effect on the menu on the mobile version of
+// the site.
+function toggleNavMenu() {
+    var menuEl = document.getElementById('navigation-menu');
+
+    if(menuEl.className == "nav-menu") {
+        menuEl.className = "nav-menu--collapsed";
+    } else {
+        menuEl.className = "nav-menu";
+    }
+}
