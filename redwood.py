d2b5f0fe2aef4c9507512c4f9d6ebacf865c9f58
Henrik Treadup <henrik.treadup@gmail.com>
2017-09-08 04:30:01 +0200

    Moved S3 related functions to storage.py module.

--- redwood.py
+++ redwood.py
@@ -2,13 +2,14 @@ from flask import Flask, render_template, request, make_response
 from flask import redirect, url_for, abort, send_file
 from werkzeug import secure_filename
 import settings
+from storage import get_s3_files_from_result, get_s3_folders_from_result
+from storage import read_s3_bucket_folder, read_s3_file, write_s3_file, read_s3_stream
 import json
 from datetime import datetime
 import time
 import pytz
 import jwt
 import hashlib
-import boto3
 from jwt import ExpiredSignatureError
 from functools import wraps
 from io import StringIO, BytesIO
@@ -443,73 +444,6 @@ def token():
 
     return render_template("token.html", token=token_jwt, user=user)
 
-def get_s3_folders_from_result(result):
-    """
-    Gets the folders from a s3 list_objects result.
-    """
-
-    if 'CommonPrefixes' in result:
-        folders = result['CommonPrefixes']
-    else:
-        folders = []
-
-    return [x['Prefix'] for x in folders]
-
-def get_s3_files_from_result(result):
-    """
-    Gets the files from a s3 list_objects result.
-    """
-    if 'Contents' in result:
-        files = result['Contents']
-    else:
-        files = []
-
-    return [x['Key'] for x in files]
-
-def read_s3_bucket_folder(bucket_name, folder):
-    """
-    Reads the contents of an s3 folder in a bucket.
-    """
-    client = boto3.client('s3')
-
-    kwargs = {'Bucket': bucket_name, 'Delimiter':'/'}
-
-    if(folder != '/'):
-        kwargs['Prefix'] = folder
-    
-    result = client.list_objects(**kwargs)
-
-    folders = get_s3_folders_from_result(result)
-    files = get_s3_files_from_result(result)
-
-    return folders, files
-
-def read_s3_file(bucket_name, key, binary=False):
-    """
-    Reads the contents of an S3 text file.
-    """
-    client = boto3.client('s3')
-
-    result = client.get_object(Bucket=bucket_name, Key=key)
-
-    if result['ResponseMetadata']['HTTPStatusCode'] != 200:
-        pass
-
-    if binary:
-        return result['Body'].read()
-    else:
-        return result['Body'].read().decode('utf-8')
-
-def write_s3_file(bucket_name, key, content):
-    """
-    Writes text to an S3 text file.
-    """
-    client = boto3.client('s3')
-    file_like_content = BytesIO(content)
-    result = client.put_object(Bucket=bucket_name, Key=key, Body=file_like_content)
-
-    # TODO: Some kind of error handling.
-
 def process_folders(folders):
     """
     Given a list of folder names create a list of folder dicts
@@ -672,25 +606,6 @@ def mimetype_from_extension(filename):
     }
 
     return extensionMap.get(extension.lower(), "application/octet-stream")
-
-def read_s3_stream(bucket_name, key):
-    """
-    Returns a stream for an S3 file.
-    """
-    client = boto3.client('s3')
-
-    result = client.get_object(Bucket=bucket_name, Key=key)
-
-    if result['ResponseMetadata']['HTTPStatusCode'] != 200:
-        pass
-
-    return result['Body']
-
-def write_s3_stream(bucket_name, key, stream):
-    """
-    Writes a stream to S3.
-    """
-    pass
 
 @app.route('/public/<token>/<filename>')
 def public_files(token, filename):
---
+++ storage.py
@@ -0,0 +1,87 @@
+import boto3
+
+def get_s3_folders_from_result(result):
+    """
+    Gets the folders from a s3 list_objects result.
+    """
+
+    if 'CommonPrefixes' in result:
+        folders = result['CommonPrefixes']
+    else:
+        folders = []
+
+    return [x['Prefix'] for x in folders]
+
+def get_s3_files_from_result(result):
+    """
+    Gets the files from a s3 list_objects result.
+    """
+    if 'Contents' in result:
+        files = result['Contents']
+    else:
+        files = []
+
+    return [x['Key'] for x in files]
+
+def read_s3_bucket_folder(bucket_name, folder):
+    """
+    Reads the contents of an s3 folder in a bucket.
+    """
+    client = boto3.client('s3')
+
+    kwargs = {'Bucket': bucket_name, 'Delimiter':'/'}
+
+    if(folder != '/'):
+        kwargs['Prefix'] = folder
+
+    result = client.list_objects(**kwargs)
+
+    folders = get_s3_folders_from_result(result)
+    files = get_s3_files_from_result(result)
+
+    return folders, files
+
+def read_s3_file(bucket_name, key, binary=False):
+    """
+    Reads the contents of an S3 text file.
+    """
+    client = boto3.client('s3')
+
+    result = client.get_object(Bucket=bucket_name, Key=key)
+
+    if result['ResponseMetadata']['HTTPStatusCode'] != 200:
+        pass
+
+    if binary:
+        return result['Body'].read()
+    else:
+        return result['Body'].read().decode('utf-8')
+
+def write_s3_file(bucket_name, key, content):
+    """
+    Writes text to an S3 text file.
+    """
+    client = boto3.client('s3')
+    file_like_content = BytesIO(content)
+    result = client.put_object(Bucket=bucket_name, Key=key, Body=file_like_content)
+
+    # TODO: Some kind of error handling.
+
+def read_s3_stream(bucket_name, key):
+    """
+    Returns a stream for an S3 file.
+    """
+    client = boto3.client('s3')
+
+    result = client.get_object(Bucket=bucket_name, Key=key)
+
+    if result['ResponseMetadata']['HTTPStatusCode'] != 200:
+        pass
+
+    return result['Body']
+
+def write_s3_stream(bucket_name, key, stream):
+    """
+    Writes a stream to S3.
+    """
+    pass
