c9d2b8dc0c2fa16a3a43f41e3f2744dd2a812d03
Henrik Treadup <henrik.treadup@gmail.com>
2017-11-12 21:36:58 +0100

    Better validation of the bookmarks.json file

--- validate.py
+++ validate.py
@@ -39,13 +39,22 @@ def validate_category(category):
         pprint(category)
         exit(1)
 
-data = load_data()
+def validate_bookmark_file():
+    try:
+        data = load_data()
 
-if not isinstance(data, list):
-    print("Root element is not a list.")
-    exit(1)
+        if not isinstance(data, list):
+            print("Root element is not a list.")
+            exit(1)
 
-for category in data:
-    validate_category(category)
+            for category in data:
+                validate_category(category)
+    except Exception as e:
+        print("There was an exception when validating the bookmarks.")
+        print(e)
+        exit(1)
+
+validate_bookmark_file()
 
 print("Validation succeeded.")
+exit(0)
